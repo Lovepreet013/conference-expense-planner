cabf25fb90c174d268c511c3992b08c924e35a63
Lovepreet013 <singhlovepreet1209@gmail.com>
2025-09-16 11:56:03 +0530

    venue section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import ConferenceEvent from "./components/ConferenceEvent"
+
 function App() {
   return (
-    <>Hello</>
+    <>
+      <main className="text-center mt-[15em] h-[100vh]">
+        <h1 className="text-6xl font-bold font-serif">Your go to Conference Expense Planner</h1>
+        <button className="my-[2em] border-2 px-5 py-2 rounded-3xl cursor-pointer bg-blue-400 hover:bg-blue-300 font-bold">Get Started</button>
+      </main>
+
+      <section>
+        <ConferenceEvent/>
+      </section>
+    </>
   )
 }
 
--- src/components/ConferenceEvent.jsx
+++ src/components/ConferenceEvent.jsx
@@ -1,6 +1,71 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrementQuantity, incrementQuantity } from "../slices/venueSlice"
+
 const ConferenceEvent = () => {
+    const dispatch = useDispatch()
+    
+    const venueItems = useSelector((state) => state.venue) //'venue' name comes from the store.js 'venu' name
+
+    //venue functions
+    const handleAddToCart = (index) => {
+        dispatch(incrementQuantity(index))
+    }
+
+    const handleRemoveFromCart = (index) => {
+        if(venueItems[index].quantity > 0){
+            dispatch(decrementQuantity(index))
+        }
+    }
+
+
+    const calculateTotalCost = (section) => {
+        let totalCost = 0
+        if(section === 'venue'){
+            venueItems.forEach((item) => {
+                totalCost += item.cost * item.quantity
+            })
+        }
+
+        return totalCost
+    }
+
+    //Total Cost
+    const venuTotalCost = calculateTotalCost('venue')
+
     return(
-        <>Conference Event</>
+        <div>
+            <section>
+                <h2 className="text-5xl">Venues</h2>
+                <div className="flex flex-wrap gap-2">
+                    {venueItems.map((item, index) => (
+                        <div key={index} className="w-[350px] h-[400px] border-1" >
+                            <img src={item.img} alt={item.name} />
+                            <h3>{item.name}</h3>
+
+                            <h4>${item.cost}</h4>
+
+                            <div className="flex justify-evenly items-center">
+                                <button 
+                                    className="border px-3 py-1 text-2xl cursor-pointer"
+                                    onClick={() => handleRemoveFromCart(index)}
+                                >   
+                                    -
+                                </button>
+                                <h4>{item.quantity}</h4>
+                                <button 
+                                    className="border px-3 py-1 text-2xl cursor-pointer"
+                                    onClick={() => handleAddToCart(index)}
+                                >
+                                    +
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <h3 className="text-center my-5 p-2 border-1">Total Cost : {venuTotalCost}</h3>
+            </section>
+        </div>
     )
 }
 
